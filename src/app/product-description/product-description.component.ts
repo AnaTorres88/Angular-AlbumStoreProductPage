62c432b1af051fb2ddb95f27566e039df9c9de8b
Ana Torres Gonzalez <ana.torres.gonzalez@accenture.com>
2018-03-26 15:46:07 -0600

    Changes module 3

--- src/app/product-description/product-description.component.ts
+++ src/app/product-description/product-description.component.ts
@@ -13,5 +13,6 @@ export class ProductDescriptionComponent implements OnInit {
   ngOnInit() {
 		this._productService.getAlbum(1).subscribe(response => this.albumInfo = response);
   }
+	console.log(albumInfo);
 
 }
